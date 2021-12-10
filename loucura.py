8984d28d3ae1fd85e07c70541606623c06ee0fa6
Adsonlima12 <66745978+Adsonlima12@users.noreply.github.com>
2021-12-10 19:00:15 -0300

    Add files via upload

---
+++ loucura.py
@@ -0,0 +1,5 @@
+print ("Hello apapi")
+otaku = input('Fale o teu anime preferido nhyaa: ')
+print(otaku,"?")
+fujoshi= input('pessimo gosto, escolha outro: ' )
+print(fujoshi,' né ? Realmente teu gosto é péssimo ' )
