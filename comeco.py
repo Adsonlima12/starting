b2a0c79785df6e1cddc6ca4c34379c6d33306241
Adsonlima12 <66745978+Adsonlima12@users.noreply.github.com>
2021-12-10 19:03:06 -0300

    Update and rename loucura.py to comeco.py

---
+++ comeco.py
@@ -0,0 +1,5 @@
+print ("Hello apapi")
+c0 = input('Fale o teu anime preferido nhyaa: ')
+print(c0,"?")
+c1= input('pessimo gosto, escolha outro: ' )
+print(c1,' né ? Realmente teu gosto é péssimo ' )
--- loucura.py
+++
@@ -1,5 +0,0 @@
-print ("Hello apapi")
-otaku = input('Fale o teu anime preferido nhyaa: ')
-print(otaku,"?")
-fujoshi= input('pessimo gosto, escolha outro: ' )
-print(fujoshi,' né ? Realmente teu gosto é péssimo ' )
